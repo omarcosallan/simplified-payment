ca27449f5f3b9fd2e8948ddb495169cf1b05de7e
Marcos Allan <marcosallanm1to@gmail.com>
2025-12-07 06:59:28 -0300

    feat: first commit

---
+++ src/main/java/com/marcos/simplified_payment/SimplifiedPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.simplified_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/simplified_payment/SimplifiedPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.simplified_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
